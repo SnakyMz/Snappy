c1842a6455d174dd2eeaf328d2ababd05528084e
SnakyMz <Soban.syeed@outlook.com>
2024-01-01 20:33:59 +0530

    Add background colors

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <section className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </section>
       </body>
     </html>
   )
