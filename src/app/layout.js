005c67295d0d590418db1256d274a712eebd4d3d
SnakyMz <Soban.syeed@outlook.com>
2024-01-01 21:01:08 +0530

    Add fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from 'next/font/google'
+import { Handlee } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/navbar'
 import Footer from '@/components/footer/footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const handlee = Handlee({ subsets: ["latin"], weight: "400" })
 
 export const metadata = {
   title: 'Snappy',
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={handlee.className}>
         <section className="container">
           <Navbar />
           {children}
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import { Lobster_Two } from "next/font/google";
+import styles from "./navbar.module.css";
+
+const lobster = Lobster_Two({ subsets: ['latin'], weight: "700" });
 
 export default function Navbar() {
   return (
-    <nav>
-      <div>Snappy</div>
-      <div>
+    <header className={styles.heading}>
+      <div className={`${lobster.className} ${styles.logo}`}>Snappy</div>
+      <nav className={styles.navigation}>
         <Link href="/">Homepage</Link>
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
         <Link href="/posts">Posts</Link>
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
