fe661f2b11514eb9520d296b5f19cad77a66102e
SnakyMz <Soban.syeed@outlook.com>
2024-01-03 14:30:38 +0530

    Add About page design

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,6 +7,8 @@ export default function About() {
       <div className={styles.desc}>
         <h1>About Us</h1>
         <p>We help people connect online.</p>
+        <p>Our goal is to help the people around the world to share each other's culture and lifestyles through pictures. Since pictures tell a story of their own. Our website allows people to share pictures and talk about it with others.</p>
+        <p>Let's connect with the WORLD!</p>
       </div>
       <div className={styles.imageDiv}>
         <Image src="/social.png" alt="Women Scrolling the app" fill />
